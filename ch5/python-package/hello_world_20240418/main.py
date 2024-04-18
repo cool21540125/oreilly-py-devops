50037d8b9861ce7ed97c9cecee122e9fd9ef141f
Tony Chou <cool21540125@gmail.com>
2024-04-18 13:56:27 +0800

    ch5 - 優化 setup.py 及 PyPI 呈現 && 更新 pkg version

--- ch5/python-package/hello_world_20240418/main.py
+++ ch5/python-package/hello_world_20240418/main.py
@@ -1,2 +1,6 @@
 def useless_fn():
     return "Hello World"
+
+
+def useless_fn2():
+    return "Hello World v2"
--- ch5/python-package/setup.py
+++ ch5/python-package/setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README", "r") as ff:
+    long_description = ff.read()
+
 setup(
     name="hello-world-this-20240418",  # 上傳到 PyPI 以後, 必須是 unique
-    version="0.0.1",
+    version="0.0.2",
     author="TonyChou",
     author_email="cool21540125@gmail.com",
     url="https://github.com/cool21540125/oreilly-py-devops",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description="A hello-world example package",
     packages=find_packages(),
     classifiers=[  # 底下這些稱之為 trove classifier, 有助於上傳到 PyPI 時, 能夠被有效的匹配 (約定成俗的 Tags 啦)
