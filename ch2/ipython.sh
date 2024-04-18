1c97e8657f70856999730256bf75839204597bd9
Tony Chou <cool21540125@gmail.com>
2024-04-18 17:23:24 +0800

    ipython

---
+++ ch2/ipython.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+exit 0
+# -----------------------------------
+
+###
+pip install ipython
+
+ipython
+
+### 進入 ipython repl
+
+### (ipython repl) 可使用 linux 指令
+!ls -l
+
+%pip list --format=json
+
+###
+
